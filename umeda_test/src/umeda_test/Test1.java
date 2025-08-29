598d2ae400ebbdd7bb93b397cfa9ac8139ce76a9
ken <ken@umeda-zosho-014>
2025-08-29 16:56:51 +0900

    蓮井　再プッシュ

--- umeda_test/src/umeda_test/Test1.java
+++ umeda_test/src/umeda_test/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.print("aaa");
+		HasuiTest hTest = new HasuiTest();
+		hTest.hasuitest();
 	}
 
 }
