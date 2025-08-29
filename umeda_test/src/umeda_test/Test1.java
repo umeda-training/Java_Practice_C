8eb1203b35137d37e5d7e6c491fdcd75e6720ae5
ken <ken@umeda-zosho-014>
2025-08-29 17:13:00 +0900

    合算コミット

--- umeda_test/src/umeda_test/Test1.java
+++ umeda_test/src/umeda_test/Test1.java
@@ -4,7 +4,19 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.print("aaa");
+		System.out.println("aaa");
+		HasuiTest hTest = new HasuiTest();
+		hTest.hasuitest();
+		
+		Sample sample = new Sample();
+		sample.print();
+		
+		Ikeda1 ikeda = new Ikeda1();
+		ikeda.print();
+		
+		ft_test fTest = new ft_test();
+		fTest.print();
+		
 	}
 
 }
